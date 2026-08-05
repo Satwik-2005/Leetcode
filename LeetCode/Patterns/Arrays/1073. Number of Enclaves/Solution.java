44e9585b6a86e63ab6afda154e1acf4c0fc573e1
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-05 19:40:20 +0530

    Add Java Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/1073. Number of Enclaves/Solution.java
+++ LeetCode/Patterns/Arrays/1073. Number of Enclaves/Solution.java
@@ -1,57 +1,66 @@
 class Pair {
-    int first;
-    int second;
+    int row;
+    int col;
 
-    public Pair(int first , int second) {
-        this.first = first;
-        this.second = second;
+    public Pair(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 }
+
 
 class Solution {
     public int numEnclaves(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
 
-        int [][]visited = new int[n][m];
+        int[][] visited = new int[n][m];
         Queue<Pair> queue = new LinkedList<>();
 
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(i == 0  ||  i == n - 1  ||  j == 0  ||  j == m - 1) {
-                    if(grid[i][j] == 1) {
-                        queue.offer(new Pair(i , j));
-                        visited[i][j] = 1;
+        for(int row=0;row<n;row++) {
+            for(int col=0;col<m;col++) {
+                if(row == 0  ||  row == n - 1  ||  col == 0  ||  col == m - 1) {
+                    if(grid[row][col] == 1) {
+                        visited[row][col] = 1;
+                        queue.offer(new Pair(row, col));
                     }
                 }
             }
         }
 
-        int []delRow = {-1 , 0 , 1 , 0};
-        int []delCol = {0 , 1 , 0 , -1};
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, 1, 0, -1};
 
         while(!queue.isEmpty()) {
-            int row = queue.peek().first;
-            int col = queue.peek().second;
-            queue.poll();
+            Pair node = queue.poll();
+
+            int row = node.row;
+            int col = node.col;
 
             for(int i=0;i<4;i++) {
-                int nRow = row + delRow[i];
-                int nCol = col + delCol[i];
+                int newRow = row + delRow[i];
+                int newCol = col + delCol[i];
 
-                if(nRow >= 0  &&  nRow < n  &&  nCol >= 0  &&  nCol < m  &&  visited[nRow][nCol] == 0  &&  grid[nRow][nCol] == 1) {
-                    queue.offer(new Pair(nRow , nCol));
-                    visited[nRow][nCol] = 1;
+                if(
+                    newRow >= 0  && newCol >= 0  &&
+                    newRow < n  &&  newCol < m  &&
+                    visited[newRow][newCol] == 0  &&
+                    grid[newRow][newCol] == 1
+                ) {
+                    visited[newRow][newCol] = 1;
+                    queue.offer(new Pair(newRow, newCol));
                 }
             }
         }
 
-        int count = 0;
-        for(int i=0;i<n;i++) 
-            for(int j=0;j<m;j++)
-                if(grid[i][j] == 1  &&  visited[i][j] == 0)
-                    count++;
+        int cnt = 0;
 
-        return count;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(grid[i][j] == 1  && visited[i][j] == 0)
+                    cnt += 1;
+
+
+        return cnt;
     }
 }
