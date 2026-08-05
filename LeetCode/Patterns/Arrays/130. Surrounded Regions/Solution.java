81da2317579fd9855d859c96ea088596df23064a
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-05 19:20:53 +0530

    Add Java Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/130. Surrounded Regions/Solution.java
+++ LeetCode/Patterns/Arrays/130. Surrounded Regions/Solution.java
@@ -1,17 +1,22 @@
 class Solution {
 
-    static void dfs(int row , int col , int [][]visited , char [][]board , int []delRow , int []delCol) {
-        visited[row][col] = 1;
-
+    private void dfs(int row, int col, char[][] board, int[][] visited, int[] delRow, int[] delCol) {
         int n = board.length;
         int m = board[0].length;
 
-        for(int i=0;i<4;i++) {
-            int nRow = row + delRow[i];
-            int nCol = col + delCol[i];
+        visited[row][col] = 1;
 
-            if(nRow >= 0  &&  nRow < n  &&  nCol >= 0  &&  nCol < m  && visited[nRow][nCol] == 0  &&  board[nRow][nCol] == 'O')
-                dfs(nRow , nCol , visited , board , delRow , delCol);
+        for(int i=0;i<4;i++) {
+            int newRow = row + delRow[i];
+            int newCol = col + delCol[i];
+
+            if(
+                newRow >= 0  &&  newCol >= 0  &&
+                newRow < n  &&  newCol < m  && 
+                visited[newRow][newCol] == 0  &&
+                board[newRow][newCol] == 'O'
+            ) 
+                dfs(newRow, newCol, board, visited, delRow, delCol);
         }
     }
 
@@ -19,29 +24,30 @@ class Solution {
         int n = board.length;
         int m = board[0].length;
 
-        int [][]visited = new int[n][m];
-        int []delRow = {-1 , 0 , 1 , 0};
-        int []delCol = {0 , 1 , 0 , -1};
+        int[][] visited = new int[n][m];
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, 1, 0, -1};
 
         for(int j=0;j<m;j++) {
             if(visited[0][j] == 0  &&  board[0][j] == 'O')
-                dfs(0 , j , visited , board , delRow , delCol);
+                dfs(0, j, board, visited, delRow, delCol);
 
             if(visited[n - 1][j] == 0  &&  board[n - 1][j] == 'O')
-                dfs(n - 1 , j , visited , board , delRow , delCol);
+                dfs(n - 1, j, board, visited, delRow, delCol);
         }
 
         for(int i=0;i<n;i++) {
             if(visited[i][0] == 0  &&  board[i][0] == 'O')
-                dfs(i , 0 , visited , board , delRow , delCol);
+                dfs(i, 0, board, visited, delRow, delCol);
 
-            if(visited[i][m - 1] == 0  &&  board[i][m - 1] == 'O')
-                dfs(i , m - 1 , visited , board , delRow , delCol);
+            if(visited[i][n - 1] == 0  &&  board[i][n - 1] == 'O')
+                dfs(i, n - 1, board, visited, delRow, delCol);
         }
 
-        for(int i=0;i<n;i++)
+
+        for(int i=0;i<n;i++) 
             for(int j=0;j<m;j++)
-                if(visited[i][j] == 0  &&  board[i][j] == 'O')
+                if(board[i][j] == 'O'  && visited[i][j] == 0)
                     board[i][j] = 'X';
     }
 }
