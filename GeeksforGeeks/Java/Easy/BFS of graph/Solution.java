5bd15a35ff29b3703b4d29db12b2cfe6c611e78c
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-05 13:16:17 +0530

    Add Easy Java Solution from GeeksforGeeks

---
+++ GeeksforGeeks/Java/Easy/BFS of graph/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        
+        int V = adj.size();
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        boolean[] visited = new boolean[V];
+        Queue<Integer> queue = new LinkedList<>();
+        
+        queue.offer(0);
+        visited[0] = true;
+        
+        while(!queue.isEmpty()) {
+            int val = queue.poll();
+            list.add(val);
+            
+            for(int it : adj.get(val)) {
+                if(visited[it] == false) {
+                    queue.offer(it);
+                    visited[it] = true;
+                }
+            }
+        }
+        
+        return list;
+    }
+}
