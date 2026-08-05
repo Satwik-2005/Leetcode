b3feb48df7b6822b8e32fd50c7c63e320009b0b1
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-05 16:06:06 +0530

    Add Java Solution to Patterns/Arrays

--- LeetCode/Patterns/Arrays/1036. Rotting Oranges/Solution.java
+++ LeetCode/Patterns/Arrays/1036. Rotting Oranges/Solution.java
@@ -3,7 +3,7 @@ class Trivial {
     int col;
     int time;
 
-    public Trivial(int row , int col , int time) {
+    public Trivial(int row, int col, int time) {
         this.row = row;
         this.col = col;
         this.time = time;
@@ -14,53 +14,58 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-
+        
         Queue<Trivial> queue = new LinkedList<>();
-        int [][]visited = new int[n][m];
+        int[][] visited = new int[n][m];
         int countFresh = 0;
 
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
                 if(grid[i][j] == 2) {
-                    queue.offer(new Trivial(i , j , 0));
+                    queue.offer(new Trivial(i, j, 0));
                     visited[i][j] = 2;
                 }
-                
+
                 if(grid[i][j] == 1)
                     countFresh += 1;
             }
         }
 
-        int []delRow = {-1 , 0 , 1 , 0};
-        int []delCol = {0 , 1 , 0 , -1};
+        int time = 0;
         int count = 0;
-        int tm = 0;
+        
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, 1, 0, -1};
 
         while(!queue.isEmpty()) {
-            int row = queue.peek().row;
-            int col = queue.peek().col;
-            int time = queue.peek().time;
+            Trivial node = queue.poll();
 
-            tm = Math.max(tm , time);
+            int row = node.row;
+            int col = node.col;
+            int tm = node.time;
 
-            queue.poll();
+            time = Math.max(tm, time);
 
             for(int i=0;i<4;i++) {
                 int nRow = row + delRow[i];
                 int nCol = col + delCol[i];
 
-                if(nRow >= 0  &&  nRow < n  &&  nCol >= 0  &&  nCol < m 
-                    &&  visited[nRow][nCol] == 0  &&  grid[nRow][nCol] == 1) {
-                        queue.offer(new Trivial(nRow , nCol , tm + 1));
-                        visited[nRow][nCol] = 2;
-                        count++;
-                    }
+                if(
+                    nRow >= 0  &&  nCol >= 0  &&  
+                    nRow < n  &&  nCol < m  &&
+                    visited[nRow][nCol] == 0  &&
+                    grid[nRow][nCol] == 1
+                ) {
+                    visited[nRow][nCol] = 2;
+                    count += 1;
+                    queue.offer(new Trivial(nRow, nCol, tm + 1));
+                }
             }
         }
 
-        if(count != countFresh) 
+        if(count != countFresh)
             return -1;
 
-        return tm;
+        return time;
     }
 }
