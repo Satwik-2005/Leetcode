16ba4732633e88e572d60ff2d4031d8dcad0b187
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-08 18:56:54 +0530

    Add Uncategorized Java Solution from GeeksforGeeks

--- GeeksforGeeks/Java/Uncategorized/Problem Solved Successfully/Solution.java
+++ GeeksforGeeks/Java/Uncategorized/Problem Solved Successfully/Solution.java
@@ -1,25 +1,36 @@
 class Solution {
-    public int countMinOperations(int arr[]) {
-        // code here
-       int dbl=0;
-        int sin=0;
-        int n=arr.length;
-        for(int ind=0;ind<n;ind++){
-            int cnt=0;
-            int val=arr[ind];
-            while(val!=0){
-                if(val%2!=0){
-                    sin++;
-                    val--;
-                }
-                else{
-                    cnt++;
-                    val/=2;
-                }
+    
+    public void dfs(ArrayList<ArrayList<Integer>> adj,boolean[] vis,int src){
+        vis[src] = true;
+        for(Integer v:adj.get(src)){
+            if(!vis[v]){
+                dfs(adj,vis,v);
             }
-            dbl=Math.max(dbl,cnt);
         }
-        return dbl+sin;
-
+    }
+    int minEdgesReq(int n, int[][] edges) {
+        // code here
+        if(edges.length < n-1) return -1;
+       
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+         for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] e : edges){
+            int a = e[0];
+            int b = e[1];
+            adj.get(a).add(b);
+            adj.get(b).add(a);
+        }
+        boolean[] vis = new boolean[n];
+        int com = 0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                vis[i] = true;
+                com++;
+                dfs(adj,vis,i);
+            }
+        }
+    return com-1;
     }
 }
