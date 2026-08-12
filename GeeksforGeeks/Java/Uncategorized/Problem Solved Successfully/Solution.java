3e71fd62d8df5c9e2f682ed558c9fb3cdd4d8b23
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-12 18:34:28 +0530

    Add Uncategorized Java Solution from GeeksforGeeks

--- GeeksforGeeks/Java/Uncategorized/Problem Solved Successfully/Solution.java
+++ GeeksforGeeks/Java/Uncategorized/Problem Solved Successfully/Solution.java
@@ -1,31 +1,40 @@
 class Solution {
-
- 
-
-    public int maxTask(int[] h, int[] l) {
-
-        int n = h.length;
-
-        int prev2 = 0; // Represents dp[i]
-
-        int prev1 = 0; // Represents dp[i+1]
-
+    public ArrayList<Integer> findWays(int[][] grid) {
+        // Code here
+        int n = grid.length;
+        int mod = 1000000007;
         
-
+        int[][] path = new int[n][n];
+        int[][] maxAdventure = new int[n][n];
+        
+        path[0][0] = 1;
+        maxAdventure[0][0] = grid[0][0];
+        
         for (int i = 0; i < n; i++) {
-
-            int curr = Math.max(h[i] + prev2, l[i] + prev1);
-
-            prev2 = prev1;
-
-            prev1 = curr;
-
+            for (int j = 0; j < n; j++) {
+                int max1 = 0;
+                int max2 = 0;
+                
+                if (i != 0 && (grid[i-1][j] == 2 || grid[i-1][j] == 3)) {
+                    path[i][j] = (path[i][j] + path[i-1][j] % mod) % mod;
+                    max1 = maxAdventure[i-1][j];
+                }
+                
+                if (j != 0 && (grid[i][j-1] == 1 || grid[i][j-1] == 3)) {
+                    path[i][j] = (path[i][j] + path[i][j-1] % mod) % mod;
+                    max2 = maxAdventure[i][j-1];
+                }
+                
+                if (max1 != 0 || max2 != 0) {
+                    maxAdventure[i][j] = Math.max(max1, max2) + grid[i][j];
+                }
+            }
         }
-
         
-
-        return prev1;    
-
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(path[n-1][n-1]);
+        ans.add(maxAdventure[n-1][n-1]);
+        
+        return ans;
     }
-
 }
