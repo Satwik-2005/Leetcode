ea49b4442ee9f68807391a23bc3f4b136db8a058
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-16 20:31:21 +0530

    Add Easy SQL Solution from LeetCode

--- LeetCode/SQL/Easy/1908. Recyclable and Low Fat Products/Solution.sql
+++ LeetCode/SQL/Easy/1908. Recyclable and Low Fat Products/Solution.sql
@@ -1,3 +1,7 @@
 # Write your MySQL query statement below
-select product_id from products
-where low_fats = 'Y' and recyclable = 'Y';
+SELECT product_id 
+FROM Products
+WHERE (
+    low_fats = 'Y' AND 
+    recyclable = 'Y'
+);
