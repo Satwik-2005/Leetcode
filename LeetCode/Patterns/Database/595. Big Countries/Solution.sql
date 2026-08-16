121babc3a65fc24b63e26fb0ede155534a4a8e97
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-16 20:47:32 +0530

    Add SQL Solution to Patterns/Database

--- LeetCode/Patterns/Database/595. Big Countries/Solution.sql
+++ LeetCode/Patterns/Database/595. Big Countries/Solution.sql
@@ -1,3 +1,8 @@
 # Write your MySQL query statement below
-select name , population , area from World
-where area >= 3000000 or population >= 25000000;
+SELECT name, population, area
+FROM World
+WHERE (
+    population >= 25000000 
+    OR
+    area >= 3000000
+);
