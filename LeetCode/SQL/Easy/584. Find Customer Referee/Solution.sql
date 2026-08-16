f691a2efbcd06a31ccc73ae62915adfb513cf76f
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-16 20:38:03 +0530

    Add Easy SQL Solution from LeetCode

--- LeetCode/SQL/Easy/584. Find Customer Referee/Solution.sql
+++ LeetCode/SQL/Easy/584. Find Customer Referee/Solution.sql
@@ -1,3 +1,8 @@
 # Write your MySQL query statement below
-select name from customer
-where referee_id <> 2 or referee_id is null;
+SELECT name
+FROM Customer
+WHERE (
+    referee_id IS null
+    OR
+    referee_id <> 2
+);
