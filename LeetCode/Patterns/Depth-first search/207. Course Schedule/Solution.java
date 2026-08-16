a55522a2fba3b07cc1067d931815033faa9b4aaf
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-16 13:07:42 +0530

    Add Java Solution to Patterns/Depth-first search

---
+++ LeetCode/Patterns/Depth-first search/207. Course Schedule/Solution.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    static void adjacencyList(List<List<Integer>> adj , int [][]prerequisites , int numCourses) {
+        for(int i=0;i<numCourses;i++)
+            adj.add(new ArrayList<>());
+
+        for(int i=0;i<prerequisites.length;i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            adj.get(u).add(v);
+        }
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+
+        adjacencyList(adj , prerequisites , numCourses);
+
+        int []inDegree = new int[numCourses];
+
+        for(int i=0;i<numCourses;i++)
+            for(int iter : adj.get(i))
+                inDegree[iter]++;
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i=0;i<numCourses;i++)
+            if(inDegree[i] == 0)
+                queue.offer(i);
+
+        int count = 0;
+
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            count++;
+
+            for(int iter : adj.get(node)) {
+                inDegree[iter]--;
+                
+                if(inDegree[iter] == 0)
+                    queue.offer(iter);
+            }
+        }
+
+        return (count == numCourses);
+    }
+}
