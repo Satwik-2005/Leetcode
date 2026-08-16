9748458f798d34db8c5352ff03f05827a439adb3
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-16 23:05:23 +0530

    Add SQL Solution to Patterns/Database

--- LeetCode/Patterns/Database/1724. Customer Who Visited but Did Not Make Any Transactions/Solution.sql
+++ LeetCode/Patterns/Database/1724. Customer Who Visited but Did Not Make Any Transactions/Solution.sql
@@ -1,7 +1,18 @@
-SELECT v.customer_id,
-       COUNT(*) AS count_no_trans
+# Write your MySQL query statement below
+-- SELECT v.customer_id, count(v.visit_id) AS count_no_trans
+-- FROM Visits v
+-- JOIN
+-- Transactions t
+-- on v.visit_id = t.visit_id
+-- WHERE t.transaction_id IS null
+-- GROUP BY v.customer_id;
+
+
+
+SELECT 
+    v.customer_id, 
+    COUNT(v.visit_id) AS count_no_trans
 FROM Visits v
-LEFT JOIN Transactions t
-ON v.visit_id = t.visit_id
+LEFT JOIN Transactions t ON v.visit_id = t.visit_id
 WHERE t.transaction_id IS NULL
 GROUP BY v.customer_id;
