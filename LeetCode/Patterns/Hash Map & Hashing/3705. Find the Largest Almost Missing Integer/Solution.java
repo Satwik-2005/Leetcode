b491f35c07fdd65d8715fefaf232da2a23340e80
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-18 09:38:18 +0530

    Add Java Solution to Patterns/Hash Map & Hashing

---
+++ LeetCode/Patterns/Hash Map & Hashing/3705. Find the Largest Almost Missing Integer/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+        Map<Integer,Integer> hashMap = new HashMap<>();
+        int n = nums.length;
+        for(int i=0;i<=n-k;i++){
+            Set<Integer> hashSet = new HashSet<>();
+            for(int j=i;j<i+k;j++){
+                if(!hashSet.contains(nums[j])){
+                    hashMap.put(nums[j],hashMap.getOrDefault(nums[j],0)+1);
+                    hashSet.add(nums[j]);
+                }
+            }
+        }
+        int missing = -1;
+        for(Map.Entry<Integer,Integer> ele : hashMap.entrySet())
+            if(ele.getValue() == 1)
+                missing = Math.max(missing,ele.getKey());
+        return missing;
+    }
+}
