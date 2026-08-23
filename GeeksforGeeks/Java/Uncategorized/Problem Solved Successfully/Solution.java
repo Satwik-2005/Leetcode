9bcaab208d1b81e64a847d541314532d91fa6194
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-08-23 13:19:12 +0530

    Add Uncategorized Java Solution from GeeksforGeeks

--- GeeksforGeeks/Java/Uncategorized/Problem Solved Successfully/Solution.java
+++ GeeksforGeeks/Java/Uncategorized/Problem Solved Successfully/Solution.java
@@ -1,36 +1,119 @@
-/* Structure of binary tree node
-class Node
-{
-    int data;
-    Node left, right;
+// class DisjointSet {
+//     List<Integer> rank = new ArrayList<>();
+//     List<Integer> parent = new ArrayList<>();
+    
+//     public DisjointSet(int n) {
+//         for(int i=0;i<=n;i++) {
+//             rank.add(0);
+//             parent.add(i);
+//         }
+//     }
+    
+//     public void unionByRank(int u, int v) {
+//         int ultimateParentOfU = findParent(u);
+//         int ultimateParentOfV = findParent(v);
+        
+//         if(ultimateParentOfU == ultimateParentOfV)
+//             return;
+            
+//         if(rank.get(ultimateParentOfU) < rank.get(UltimateParentOfV))
+//             parent.set(ultimateParentOfU, UltimateParentOfV);
+            
+//         else if(rank.get(ultimateParentOfU) > rank.get(UltimateParentOfV))
+//             parent.set(UltimateParentOfV, ultimateParentOfU);
+            
+//         else {
+//             parent.set(UltimateParentOfV, ultimateParentOfU);
+            
+//             int rankU = rank.get(ultimateParentOfU);
+            
+//             rank.set(ultimateParentOfU, rankU + 1);
+//         }
+//     }
+    
+//     public int findParent(int node) {
+//         if(node == parent.get(node))
+//             return node;
+            
+//         int ultimateParent = findParent(parent.get(node));
+//         parent.set(node, ultimateParent);
+        
+//         return parent.get(node);
+//     }
+// }
 
-    Node(int item)
-    {
-        data = item;
-        left = right = null;
+// class Solution {
+//     public ArrayList<Integer> DSU(int n, int[][] queries) {
+//         // code here
+        
+//         DisjointSet dsu = new DisjointSet(n);
+        
+        
+//     }
+// }
+import java.util.*;
+
+class DisjointSet {
+
+    List<Integer> parent = new ArrayList<>();
+
+    public DisjointSet(int n) {
+
+        for(int i = 0; i <= n; i++)
+            parent.add(i);
+    }
+
+    public int findParent(int node) {
+
+        if(node == parent.get(node))
+            return node;
+
+        int ultimateParent = findParent(parent.get(node));
+
+        parent.set(node, ultimateParent);
+
+        return ultimateParent;
+    }
+
+    public void union(int x, int z) {
+
+        int parentX = findParent(x);
+        int parentZ = findParent(z);
+
+        if(parentX == parentZ)
+            return;
+
+        // Representative of z's group remains the representative
+        parent.set(parentX, parentZ);
     }
 }
-*/
+
 class Solution {
-     int ans = Integer.MIN_VALUE;
 
-     int dfs(Node root) {
-         if (root == null)
-             return Integer.MAX_VALUE;
+    public ArrayList<Integer> DSU(int n, int[][] queries) {
 
-         int left = dfs(root.left);
-         int right = dfs(root.right);
+        DisjointSet dsu = new DisjointSet(n);
 
-         int min = Math.min(left, right);
+        ArrayList<Integer> answer = new ArrayList<>();
 
-         if (min != Integer.MAX_VALUE)
-             ans = Math.max(ans, root.data - min);
+        for(int[] query : queries) {
 
-         return Math.min(root.data, min);
-     }
+            if(query[0] == 1) {
 
-     int maxDiff(Node root) {
-         dfs(root);
-         return ans;
-     }
- }
+                int x = query[1];
+                int z = query[2];
+
+                dsu.union(x, z);
+            }
+
+            else {
+
+                int x = query[1];
+
+                answer.add(dsu.findParent(x));
+            }
+        }
+
+        return answer;
+    }
+}
