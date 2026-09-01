9dabde50838b0391e4c86274c76ca4f54a644f4c
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-09-01 10:35:32 +0530

    Add Uncategorized Java Solution for Problem Solved Successfully from GeeksforGeeks

--- GeeksforGeeks/Java/Uncategorized/Problem Solved Successfully/Solution.java
+++ GeeksforGeeks/Java/Uncategorized/Problem Solved Successfully/Solution.java
@@ -1,26 +1,35 @@
-// class Solution {
-//     public ArrayList<Integer> getMarks(int[] l, int[] r, int[] rank) {
-//         // code here
-        
-//     }
-// }
-
-
-
 class Solution {
-    public ArrayList<Integer> getMarks(int[] l, int[] r, int[] rank) {
-        // code here
-        ArrayList<Integer> ans=new ArrayList<>();
-        int c=0,idx=0;
-        for(int i:rank){
-            while((c+r[idx]-l[idx]+1)<i){
-                c+=r[idx]-l[idx]+1;
-                idx++;
+
+    static final long MOD = 1_000_000_007L;
+
+    public int palindromicStrings(int n, int k) {
+
+        long ans = 0;
+
+        // perm = P(k, m)
+        // Initially P(k, 0) = 1
+        long perm = 1;
+
+        // m = number of characters in the first half
+        for (int m = 0; 2 * m <= n; m++) {
+
+            if (m > 0) {
+                perm = (perm * (k - m + 1)) % MOD;
             }
-            ans.add(l[idx]+(i-c-1));
-            c=0;
-            idx=0;
+
+            // Odd length = 2*m + 1
+            if (2 * m + 1 <= n) {
+                long oddCount = (perm * (k - m)) % MOD;
+                ans = (ans + oddCount) % MOD;
+            }
+
+            // Even length = 2*m
+            if (m > 0 && 2 * m <= n) {
+                ans = (ans + perm) % MOD;
+            }
         }
-        return ans;
+
+        return (int) ans;
     }
 }
+
