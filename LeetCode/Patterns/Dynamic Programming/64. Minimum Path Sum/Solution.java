1933c5628d9710f5c43e355e2d4370f484de0bc5
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-09-03 12:21:10 +0530

    Add Medium Java Solution for 64. Minimum Path Sum from LeetCode

--- LeetCode/Patterns/Dynamic Programming/64. Minimum Path Sum/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/64. Minimum Path Sum/Solution.java
@@ -1,26 +1,78 @@
 class Solution {
 
-    private int minimumPathSum(int row, int col, int[][] grid) {
-        if(row == 0  &&  col == 0)
-            return grid[row][col];
+    int minimumPathSum(int i , int j , int[][] grid) {
+        if(i == 0  &&  j == 0)
+            return grid[i][j];
 
-        if(row < 0  ||  col < 0)
+        if(i < 0  ||  j < 0)
             return Integer.MAX_VALUE;
 
-        int up = minimumPathSum(row - 1 , col , grid);
-        int left = minimumPathSum(row , col - 1 , grid);
+        int up = minimumPathSum(i - 1 , j , grid);
+        int left = minimumPathSum(i , j - 1 , grid);
         int min = Math.min(up , left);
 
         if(min == Integer.MAX_VALUE)
             return min;
 
-        return grid[row][col] + min;
+        return grid[i][j] + min;
+    }
+
+    int minimumPathSum(int i , int j , int[][] grid , int[][] dp) {
+        if(i == 0  &&  j == 0)
+            return grid[i][j];
+
+        if(i < 0  ||  j < 0)
+            return Integer.MAX_VALUE;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        int up = minimumPathSum(i - 1 , j , grid , dp);
+        int left = minimumPathSum(i , j - 1 , grid , dp);
+        int min = Math.min(up , left);
+
+        if(min == Integer.MAX_VALUE)
+            return min;
+
+        return dp[i][j] = grid[i][j] + min;
     }
 
     public int minPathSum(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
 
-        return minimumPathSum(n - 1, m - 1, grid);
+        int[] prev = new int[n];
+
+        for(int i=0;i<m;i++) {
+            int[] current = new int[n];
+
+            for(int j=0;j<n;j++) {
+                if(i == 0  &&  j == 0)
+                    current[j] = grid[i][j];
+
+                else {
+                    int up = grid[i][j];
+
+                    if(i > 0)
+                        up += prev[j];
+                    else
+                        up += 1e9;
+
+                    int left = grid[i][j];
+
+                    if(j > 0)
+                        left += current[j - 1];
+                    else
+                        left += 1e9;
+
+
+                    current[j] = Math.min(left , up);
+                }
+            }
+
+            prev = current;
+        }
+
+        return prev[n - 1];
     }
 }
