3feb8e911921ab904114f92e0d040e66fe9bab25
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-09-06 10:58:49 +0530

    Add Uncategorized Java Solution for Problem Solved Successfully from GeeksforGeeks

--- GeeksforGeeks/Java/Uncategorized/Problem Solved Successfully/Solution.java
+++ GeeksforGeeks/Java/Uncategorized/Problem Solved Successfully/Solution.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int maxDiffSum(int[] arr) {
-        int n = arr.length;
-        if (n == 1) return 0;
-
-        int dp0 = 0, dp1 = 0;
-
-        for (int i = 1; i < n; i++) {
-            int ndp0 = Math.max(dp0 + Math.abs(arr[i] - arr[i - 1]),
-                                dp1 + Math.abs(arr[i] - 1));
-            int ndp1 = Math.max(dp0 + Math.abs(1 - arr[i - 1]),
-                                dp1);
-            dp0 = ndp0;
-            dp1 = ndp1;
-        }
-
-        return Math.max(dp0, dp1);
+public long pairAndSum(int[] arr) {
+        long ans = 0;
+        for (int bit = 0; bit < 31; bit++) {
+           long count = 0;
+           for (int num : arr) {
+               if ((num & (1 << bit)) != 0) {
+                   count++;
+               }
+           }
+           long pairs = count * (count - 1) / 2;
+           ans += pairs * (1L << bit);
+       }
+       return ans;
     }
 }
