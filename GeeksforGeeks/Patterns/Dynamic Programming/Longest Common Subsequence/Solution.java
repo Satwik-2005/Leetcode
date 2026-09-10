3150f0648420eb55f3407781c6141a53febf9603
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-09-10 11:26:10 +0530

    Add Medium Java Solution for Longest Common Subsequence from GeeksforGeeks

---
+++ GeeksforGeeks/Patterns/Dynamic Programming/Longest Common Subsequence/Solution.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    private int longest(int index1, int index2, String s1, String s2, int[][] dp) {
+        if(index1 < 0  ||  index2 < 0)
+            return 0;
+            
+        if(dp[index1][index2] != -1)
+            return dp[index1][index2];
+            
+        if(s1.charAt(index1) == s2.charAt(index2))
+            return 1 + longest(index1 - 1, index2 - 1, s1, s2, dp);
+            
+        return dp[index1][index2] = 0 + Math.max(
+            longest(index1 - 1, index2, s1, s2, dp),
+            longest(index1, index2 - 1, s1, s2, dp)
+        );
+    }
+    
+    private int longest(int index1, int index2, String s1, String s2) {
+        if(index1 < 0  ||  index2 < 0)
+            return 0;
+            
+        if(s1.charAt(index1) == s2.charAt(index2))
+            return 1 + longest(index1 - 1, index2 - 1, s1, s2);
+            
+        return 0 + Math.max(
+            longest(index1 - 1, index2, s1, s2),
+            longest(index1, index2 - 1, s1, s2)
+        );
+    }
+    
+    public int lcs(String s1, String s2) {
+        // code here
+        int n = s1.length();
+        int m = s2.length();
+        
+        int[][] dp = new int[n][m];
+        
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+        
+        return longest(n - 1, m - 1, s1, s2, dp);
+    }
+}
