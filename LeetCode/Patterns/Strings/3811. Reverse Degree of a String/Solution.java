9be62577f7656273f12207c17f296549e4d47ad4
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-09-20 10:12:17 +0530

    Add Easy Java Solution for 3811. Reverse Degree of a String from LeetCode

---
+++ LeetCode/Patterns/Strings/3811. Reverse Degree of a String/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int degree = 0;
+        int i = 1;
+        for(char ch : s.toCharArray()){
+            int revPos = 'z' - ch + 1;
+            degree += revPos * i;
+            i++;
+        }
+        return degree;
+    }
+}
