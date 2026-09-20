fc56acd14f113220ba949c12177b8f7fd2a8c6c0
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-09-20 11:10:44 +0530

    Add Medium Java Solution for 122. Best Time to Buy and Sell Stock II from LeetCode

--- LeetCode/Java/Medium/122. Best Time to Buy and Sell Stock II/README.md
+++ LeetCode/Java/Medium/122. Best Time to Buy and Sell Stock II/README.md
@@ -8,8 +8,8 @@
 Array, Dynamic Programming, Greedy
 
 ### 🚀 Performance
-- **Runtime:** 4 ms
-- **Memory:** 49.8 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Dynamic Programming/122. Best Time to Buy and Sell Stock II/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/122. Best Time to Buy and Sell Stock II/Solution.java
@@ -1,5 +1,6 @@
 class Solution {
 
+    // 1 -> buy, 0 -> sell
     private int maximumProfit(int index, int buy, int[] prices, int[][] dp) {
         if(index >= prices.length)
             return 0;
@@ -49,11 +50,31 @@ class Solution {
 
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
+        int[][] dp = new int[n + 1][2];
 
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
+        dp[n][0] = 0;
+        dp[n][1] = 0;
 
-        return maximumProfit(0, 1, prices, dp);
+        for(int index=n-1;index>=0;index--) {
+            for(int buy=0;buy<2;buy++) {
+                int profit = 0;
+
+                if(buy == 1)
+                    profit = Math.max(
+                        -prices[index] + dp[index + 1][0],
+                        dp[index + 1][1]
+                    );
+
+                else 
+                    profit = Math.max(
+                        prices[index] + dp[index + 1][1],
+                        dp[index + 1][0]
+                    );
+
+                dp[index][buy] = profit;
+            }
+        }
+
+        return dp[0][1];
     }
 }
