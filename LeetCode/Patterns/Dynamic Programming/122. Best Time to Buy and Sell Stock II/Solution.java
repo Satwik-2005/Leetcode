4b877448f98c745c192cc3020b00f49231834f71
Satwik Reddy Kunduru <satwik925reddy@gmail.com>
2026-09-20 10:55:44 +0530

    Add Medium Java Solution for 122. Best Time to Buy and Sell Stock II from LeetCode

--- LeetCode/Patterns/Dynamic Programming/122. Best Time to Buy and Sell Stock II/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/122. Best Time to Buy and Sell Stock II/Solution.java
@@ -1,53 +1,31 @@
 class Solution {
 
-    int profit = 0;
-
-    int maxProfitOfStocks(int index , int buy , int[] prices , int n , int[][] dp) {
-        if(index == n)
+    // 1 -> buy, 0 -> sell
+    private int maximumProfit(int index, int buy, int[] prices) {
+        if(index >= prices.length)
             return 0;
-
-        if(dp[index][buy] != -1)
-            return dp[index][buy];
+        
+        int profit = 0;
 
         if(buy == 1)
-            profit = Math.max(-prices[index] + maxProfitOfStocks(index + 1 , 0 , prices , n , dp) , 
-                            0 + maxProfitOfStocks(index + 1 , 1 , prices , n , dp));
+            profit = Math.max(
+                -prices[index] + maximumProfit(index + 1, 0, prices),
+                maximumProfit(index + 1, 1, prices)
+            );
 
-        else
-            profit = Math.max(prices[index] + maxProfitOfStocks(index + 1 , 1 , prices , n , dp) , 
-                            0 + maxProfitOfStocks(index + 1 , 0 , prices , n , dp));
-
-        return dp[index][buy] = profit;
-    }
-
-    int maxProfitOfStocks(int index , int buy , int[] prices , int n) {
-        if(index == n)
-            return 0;
-
-        if(buy == 1)
-            profit = Math.max(-prices[index] + maxProfitOfStocks(index + 1 , 0 , prices , n) , 
-                            0 + maxProfitOfStocks(index + 1 , 1 , prices , n));
-
-        else
-            profit = Math.max(prices[index] + maxProfitOfStocks(index + 1 , 1 , prices , n) , 
-                            0 + maxProfitOfStocks(index + 1 , 0 , prices , n));
+        else 
+            profit = Math.max(
+                prices[index] + maximumProfit(index + 1, 1, prices),
+                maximumProfit(index + 1, 0, prices)
+            );
 
         return profit;
     }
 
+
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int aheadNotBuy = 0;
-        int aheadBuy = 0;
 
-        for(int i=n-1;i>=0;i--) {
-            int currNotBuy = Math.max((prices[i] + aheadBuy) , aheadNotBuy);
-            int currBuy = Math.max((-prices[i] + aheadNotBuy) , aheadBuy);
-
-            aheadNotBuy = currNotBuy;
-            aheadBuy = currBuy;
-        }
-
-        return aheadBuy;
+        return maximumProfit(0, 1, prices);
     }
 }
